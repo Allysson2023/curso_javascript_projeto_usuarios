c3e19a11041b54141dd7f560a2422c5909ddf7c2
Allysson <allyssoncarlos.ac21@gmail.com>
2025-08-01 13:09:17 -0300

    Pegando os dados do checked

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-var nome = document.querySelector("#exampleInputName");
-var masculino = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var nascimento = document.querySelector("#exampleInputBirth");
-var pais = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var senha = document.querySelector("#exampleInputPassword");
-var files = document.querySelector("#exampleInputFile"); 
-var admin = document.querySelector("#exampleInputAdmin");
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field){
 
     if(field.name == "gender"){
 
         if (field.checked){
-            console.log("Sim", field);
+            user[field.name] = field.value;
         }
 
         
     }else {
-        console.log("Não");
+        user[field.name] = field.value;
         
     }
 
-    //console.log(field.name);
     
-
+    
 });
+console.log(user);
