640239e07258e9036aec5d5dbaa1951092c69dc2
Allysson <allyssoncarlos.ac21@gmail.com>
2025-08-06 22:47:32 -0300

    tentando adiciona o botao edita e cancelar

--- controler/UserControler.js
+++ controler/UserControler.js
@@ -141,10 +141,19 @@ class UserControler {
             <td> ${(dataUser.admin)? 'Sim': 'Não'} </td>
             <td> ${Utils.dateFormat(dataUser.register)} </td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+            //console.log(JSON.parse(tr.dataset.user));
+            console.log(tr);
+            
+            //document.querySelector("#box-user-create").style.display = "nome";
+            //document.querySelector("box-user-update").style.display = "block";
+            
+        });
 
         this.tableEl.appendChild(tr);
 
